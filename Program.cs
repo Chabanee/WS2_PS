3dc45e90f86db048c91072525942b0bafb9470b1
SOMA <schabane5@gmail.com>
2022-11-17 17:44:20 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS1_PS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UpperCase U = new UpperCase();
+            U.changeToUpperCase();
+            Console.WriteLine("");
+
+            Console.WriteLine("Presse a key to close the cmd line.....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UpperCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS1_PS
+{
+   public class UpperCase
+    {
+        private string word;
+        private string words;
+        public void changeToUpperCase()
+        {
+            Console.WriteLine("**************************WELCOME**********************");
+            Console.WriteLine("Please enter a text: ");
+
+            word = Convert.ToString(Console.ReadLine());
+
+
+            if(word.Length < 1 || word.Length > 8)
+            {
+                Console.WriteLine("Word too long or too short, please try again.");
+            }
+            else
+            {
+                words = word.ToUpper();
+
+                Console.WriteLine(words);
+            }
+
+        }
+    }
+}
